355ece9d731e20edc15d0db7793a9fa711ae0686
HASUMI Hitoshi <hasumikin@gmail.com>
2021-04-14 13:18:18 +0900

    Fix BRACKET

--- keymap.rb
+++ keymap.rb
@@ -19,22 +19,22 @@ kbd.init_pins(
 
 # default layer should be added at first
 kbd.add_layer :default, [
-  %i(KC_ESCAPE KC_Q    KC_W    KC_E    KC_R      KC_T     KC_Y      KC_U      KC_I      KC_O     KC_P      KC_MINUS),
-  %i(KC_TAB    KC_A    KC_S    KC_D    KC_F      KC_G     KC_H      KC_J      KC_K      KC_L     KC_SCOLON KC_BSPACE),
-  %i(KC_LSFT   KC_Z    KC_X    KC_C    KC_V      KC_B     KC_N      KC_M      KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT),
-  %i(KC_NO     KC_NO   KC_NO   ALT_AT  CTL_EQ  LOWER_SPC  RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
+  %i(KC_ESCAPE KC_Q    KC_W    KC_E        KC_R      KC_T     KC_Y      KC_U      KC_I      KC_O     KC_P      KC_MINUS),
+  %i(KC_TAB    KC_A    KC_S    KC_D        KC_F      KC_G     KC_H      KC_J      KC_K      KC_L     KC_SCOLON KC_BSPACE),
+  %i(KC_LSFT   KC_Z    KC_X    KC_C        KC_V      KC_B     KC_N      KC_M      KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT),
+  %i(KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
 ]
 kbd.add_layer :raise, [
-  %i(KC_GRAVE  KC_EXLM KC_AT   KC_HASH KC_DLR    KC_PERC  KC_CIRC   KC_AMPR   KC_ASTER  KC_LPRN  KC_RPRN   KC_MIUNS),
-  %i(KC_TAB    KC_LABK KC_LCBR KC_LBRC KC_LPRN   KC_QUOTE KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE),
-  %i(KC_LSFT   KC_RABK KC_RCBR KC_RBRC KC_RPRN   KC_DQUO  KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT),
-  %i(KC_NO     KC_NO   KC_NO   ALT_AT  CTL_EQ  LOWER_SPC  RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
+  %i(KC_GRAVE  KC_EXLM KC_AT   KC_HASH     KC_DLR    KC_PERC  KC_CIRC   KC_AMPR   KC_ASTER  KC_LPRN  KC_RPRN   KC_MIUNS),
+  %i(KC_TAB    KC_LABK KC_LCBR KC_LBRACKET KC_LPRN   KC_QUOTE KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE),
+  %i(KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT),
+  %i(KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
 ]
 kbd.add_layer :lower, [
-  %i(KC_ESCAPE KC_1    KC_2    KC_3    KC_4      KC_5     KC_6      KC_7      KC_8      KC_9     KC_0      KC_MIUNS),
-  %i(KC_TAB    KC_F2   KC_F10  KC_F12  KC_LPRN   KC_QUOTE KC_DOT    KC_4      KC_5      KC_6     KC_PLUS   KC_BSPACE),
-  %i(KC_LSFT   KC_RABK KC_RCBR KC_RBRC KC_RPRN   KC_DQUO  KC_0      KC_1      KC_2      KC_3     KC_SLASH  KC_COMMA),
-  %i(KC_NO     KC_NO   KC_NO   ALT_AT  CTL_EQ  LOWER_SPC  RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
+  %i(KC_ESCAPE KC_1    KC_2    KC_3        KC_4      KC_5     KC_6      KC_7      KC_8      KC_9     KC_0      KC_MIUNS),
+  %i(KC_TAB    KC_F2   KC_F10  KC_F12      KC_LPRN   KC_QUOTE KC_DOT    KC_4      KC_5      KC_6     KC_PLUS   KC_BSPACE),
+  %i(KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_0      KC_1      KC_2      KC_3     KC_SLASH  KC_COMMA),
+  %i(KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
 ]
 #
 #                   Your custom     Keycode or             Keycode (only modifiers)             Release time      Re-push time
